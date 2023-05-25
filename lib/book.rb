5b86e5eb5e91630f75a0a3fa0dd9d053721d0842
Cassie <cassierasugu@gmail.com>
2023-05-25 08:03:45 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
+class Books
+    attr_accessor :genre, :title, :author, :description
+    def init (title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the pages... eeiish, you read fast!"
+    end
+end
+class Movies
+    
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoes 
+    attr_accessor :brand, :color :size :condition
+    def init (brand)
+        @brand = brand
+        @color = "black"
+        @condition = "new"
+    end
+    def cobble
+        puts "Your shoe is as good as new woow!"
+        self.condition = "new"
+    end
+end
+
